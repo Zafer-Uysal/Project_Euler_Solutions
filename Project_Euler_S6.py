d8313c44a61cd3d17146115f11416611fa909871
Zafer <zafer.uysal101@gmail.com>
2025-12-21 15:05:09 +0300

    6th_Solution

---
+++ Project_Euler_S6.py
@@ -0,0 +1,19 @@
+#The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385
+#The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 552 = 3025
+#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+#Find the difference between the sum of the squares of the first n natural numbers and the square of the sum.
+
+x = 0
+y = 0
+for i in range(1,101):
+    x = i**2
+    y = x + y
+
+a = 0
+for k in range(1,101):
+    a = a + k
+b = a**2
+
+result = b - y
+print("Difference between the sum of the squares of the first n natural numbers and the square of the sum:", result)
+
