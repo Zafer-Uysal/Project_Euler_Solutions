99d0cc528a266e120bfb6c16dc1db45df0e9f979
Zafer <zafer.uysal101@gmail.com>
2026-01-18 23:31:15 +0300

    11th_Solution

---
+++ C/Project_Euler_S11.c
@@ -0,0 +1,120 @@
+// Project Euler – Problem 11
+// Title: Largest Product in a Grid
+// Link: https://projecteuler.net/problem=11
+//
+// Find the greatest product of adjacent numbers
+// in the same direction in a grid.
+
+#include <stdio.h>
+
+int main() {
+    char *rows[20] = {
+        "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08",
+        "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00",
+        "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65",
+        "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91",
+        "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80",
+        "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50",
+        "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70",
+        "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21",
+        "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72",
+        "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95",
+        "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92",
+        "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57",
+        "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58",
+        "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40",
+        "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66",
+        "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69",
+        "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36",
+        "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16",
+        "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54",
+        "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
+    };
+
+    int grid[20][20];
+    for (int i = 0; i < 20; i++) {
+        sscanf(rows[i],
+            "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+            &grid[i][0], &grid[i][1], &grid[i][2], &grid[i][3], &grid[i][4],
+            &grid[i][5], &grid[i][6], &grid[i][7], &grid[i][8], &grid[i][9],
+            &grid[i][10], &grid[i][11], &grid[i][12], &grid[i][13], &grid[i][14],
+            &grid[i][15], &grid[i][16], &grid[i][17], &grid[i][18], &grid[i][19]
+        );
+    }
+
+    // Horizontal sum
+    long long max_result = 0;
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < 17; j++)
+        {
+            int x1 = grid[i][j];
+            int x2 = grid[i][j + 1];    
+            int x3 = grid[i][j + 2];
+            int x4 = grid[i][j + 3];
+            long long mult = x1 * x2 * x3 * x4;
+
+            if (mult > max_result)
+            {
+                max_result = mult;
+            }
+        }      
+    }
+
+    // Vertical sum
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < 17; j++)
+        {
+            int y1 = grid[j][i];
+            int y2 = grid[j + 1][i];
+            int y3 = grid[j + 2][i];
+            int y4 = grid[j + 3][i];
+            long long mult = (long long)y1 * y2 * y3 * y4;
+
+            if (mult > max_result)
+            {
+                max_result = mult;
+            }
+        }
+    }
+
+    // Diagonal sum
+    for (int i = 0; i < 17; i++)
+    {
+        for (int j = 0; j < 17; j++)
+        {
+            int z1 = grid[i][j];
+            int z2 = grid[i + 1][j + 1];
+            int z3 = grid[i + 2][j + 2];
+            int z4 = grid[i + 3][j + 3];
+            long long mult = (long long)z1 * z2 * z3 * z4;
+
+            if (mult > max_result)
+            {
+                max_result = mult;
+            }
+        }
+    }
+
+    // Diagonal sum 2
+    for (int i = 0; i < 17; i++)
+    {
+        for (int j = 3; j < 20; j++)
+        {
+            int d1 = grid[i][j];
+            int d2 = grid[i + 1][j - 1];
+            int d3 = grid[i + 2][j - 2];
+            int d4 = grid[i + 3][j - 3];
+            long long mult = (long long)d1 * d2 * d3 * d4;
+
+            if (mult > max_result)
+            {
+                max_result = mult;
+            }
+        }
+    }
+
+    printf("Greatest product of adjacent numbers in the same direction in a 20x20 grid: %lld", max_result);
+    return 0;
+}
